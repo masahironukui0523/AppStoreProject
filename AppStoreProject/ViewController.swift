de4065791ff7dfe8cf8ae1f5bc3bd634ca60d641
masa <mickey24352002@gmail.com>
2019-02-27 20:39:38 +0900

    Initial Commit

---
+++ AppStoreProject/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  AppStoreProject
+//
+//  Created by 抜井正寛 on 2019/02/27.
+//  Copyright © 2019 Masahiro Nukui. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
