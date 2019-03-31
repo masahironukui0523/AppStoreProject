839570bd29601aa40f1d449979f33515d6b30934
masa <mickey24352002@gmail.com>
2019-03-31 15:43:29 +0900

    Add a new animation of when cell is pressed and shadow around the cell.

---
+++ AppStoreProject/Views/Today/BaseTodatyCell.swift
@@ -0,0 +1,50 @@
+//
+//  BaseTadatyCell.swift
+//  AppStoreProject
+//
+//  Created by 抜井正寛 on 2019/03/30.
+//  Copyright © 2019 Masahiro Nukui. All rights reserved.
+//
+
+import UIKit
+
+class BaseTodayCell: UICollectionViewCell {
+    var todayItem: TodayItem!
+    
+    override var isHighlighted: Bool {
+        didSet {
+            var transform: CGAffineTransform = .identity
+            if isHighlighted {
+                transform = .init(scaleX: 0.9, y: 0.9)
+            }
+            UIView.animate(withDuration: 0.5, delay: 0, usingSpringWithDamping: 1, initialSpringVelocity: 1, options: .curveEaseOut, animations: {
+                self.transform = .identity
+                
+                self.transform = transform
+                
+            })
+        }
+    }
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        
+        self.backgroundView = UIView()
+        
+        addSubview(self.backgroundView!)
+        self.backgroundView?.fillSuperview()
+        self.backgroundView?.backgroundColor = .white
+        self.backgroundView?.layer.cornerRadius = 16
+        
+        self.backgroundView?.layer.shadowOpacity = 0.1
+        self.backgroundView?.layer.shadowRadius = 10
+        self.backgroundView?.layer.shadowOffset = .init(width: 0, height: 10)
+        self.backgroundView?.layer.shouldRasterize = true
+        
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+}
