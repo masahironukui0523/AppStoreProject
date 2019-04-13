182d771d5b3450aa859775583cf438a62bb8bfed
masa <mickey24352002@gmail.com>
2019-04-13 14:57:05 +0900

    Add the music section.

--- AppStoreProject/Controller/Music/MusicController.swift
+++ AppStoreProject/Controller/Music/MusicController.swift
@@ -8,9 +8,39 @@
 
 import UIKit
 
-class MusicController: BaseListController {
+class MusicController: BaseListController, UICollectionViewDelegateFlowLayout {
+    
+    let cellId = "musicellid"
+    let footerId = "footerId"
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         collectionView.backgroundColor = .white
+        
+        collectionView.register(TrackCell.self, forCellWithReuseIdentifier: cellId)
+        collectionView.register(MusicLoadingFooter.self, forSupplementaryViewOfKind: UICollectionView.elementKindSectionFooter, withReuseIdentifier: footerId)
     }
+    
+    override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: cellId, for: indexPath) as! TrackCell
+        return cell
+    }
+    
+    override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 20
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        return CGSize(width: view.frame.width, height: 100)
+    }
+    
+    override func collectionView(_ collectionView: UICollectionView, viewForSupplementaryElementOfKind kind: String, at indexPath: IndexPath) -> UICollectionReusableView {
+        let footer = collectionView.dequeueReusableSupplementaryView(ofKind: kind, withReuseIdentifier: footerId, for: indexPath)
+        return footer
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, referenceSizeForFooterInSection section: Int) -> CGSize {
+        return .init(width: view.frame.width, height: 100)
+    }
+    
 }
