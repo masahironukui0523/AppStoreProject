7c7010a6aa1f0aa09fae7b759f1948eaf02037ac
masa <mickey24352002@gmail.com>
2019-04-13 14:32:30 +0900

    Fix tab bar title.

---
+++ AppStoreProject/Controller/Music/MusicController.swift
@@ -0,0 +1,16 @@
+//
+//  MusicController.swift
+//  AppStoreProject
+//
+//  Created by 抜井正寛 on 2019/04/13.
+//  Copyright © 2019 Masahiro Nukui. All rights reserved.
+//
+
+import UIKit
+
+class MusicController: BaseListController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        collectionView.backgroundColor = .white
+    }
+}
